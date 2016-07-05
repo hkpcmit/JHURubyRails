cf760734f17a1a239f920dd63490e6464abfac5c
hkpcmit <calying@yahoo.com>
2016-07-04 23:40:29 -0700

    Fix rspec failures.

--- Week3/graded-assignments/course01/module03/assignment01-Recipe-Hunter/recipefinder/app/models/recipe.rb
+++ Week3/graded-assignments/course01/module03/assignment01-Recipe-Hunter/recipefinder/app/models/recipe.rb
@@ -1,12 +1,13 @@
+# coding: utf-8
 class Recipe 
     include HTTParty
     key_value = ENV['FOOD2FORK_KEY']
-    hostport = ENV[’FOOD2FORK_SERVER_AND_PORT’] || ’www.food2fork.com’
+    hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || ’www.food2fork.com’
     base_uri "http://#{hostport}/api"
     default_params key: key_value
     format :json
     
     def self.for term
-        get("/search", query: {search: term})['recipes']
+        get("/search", query: {q: term})['recipes']
     end
 end
